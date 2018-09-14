f3393e806434ef06d9c4883581c06ee66f9e0db0
Katherine Gilligan <katia.gilligan88@gmail.com>
2018-09-14 11:45:15 -0600

    Added Jokes Page component and token header validation

---
+++ client/src/Components/JokesPage.js
@@ -0,0 +1,43 @@
+import React from 'react'; 
+import axios from 'axios'; 
+
+class JokesPage extends React.Component{
+    constructor(){
+        super(); 
+        this.state  = {
+            jokes: []
+        }
+    }
+
+    componentDidMount = () => {
+        const token = localStorage.getItem('jwt'); 
+        const requestOptions = {
+            headers: {
+                Authorization: token
+            }
+        }
+        axios.get("http://localhost:3300/api/jokes", requestOptions).then(response => {
+            this.setState({
+                jokes: response.data
+            })
+        }).catch(err => {
+            console.log(err); 
+        })
+    }
+
+    signOutHandler = () => {
+        localStorage.removeItem('jwt'); 
+        this.props.history.push('/login'); 
+    }
+
+    render(){
+        return (
+            <div className = 'jokes'>
+                {this.state.jokes.map(joke => {
+                    return <li>{joke.setup} {joke.punchline}</li>
+                })}
+                <button onClick = {this.signOutHandler} >Signout</button>
+            </div>
+        )
+    }
+}
