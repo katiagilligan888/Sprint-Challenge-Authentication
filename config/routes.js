b9970d2ce530a737dce43c1ad830080207207acb
Katherine Gilligan <katia.gilligan88@gmail.com>
2018-09-14 10:11:57 -0600

    Added the register post function with validation and tokens

--- config/routes.js
+++ config/routes.js
@@ -1,4 +1,12 @@
 const axios = require('axios');
+const Joi = require('Joi'); 
+const bcrypt = require('bcryptjs'); 
+const jwt = require('jsonwebtoken'); 
+const key = require('../_secrets/keys'); 
+const dbConfig = require('../knexfile'); 
+const knex = require('knex'); 
+
+const db = knex(dbConfig.development); 
 
 const { authenticate } = require('./middlewares');
 
@@ -8,9 +16,54 @@ module.exports = server => {
   server.get('/api/jokes', authenticate, getJokes);
 };
 
-function register(req, res) {
-  // implement user registration
+
+
+function generateToken(username){
+  
+  const secret = key.jwtKey;
+  
+  const payload = {
+    username, 
+  }
+  
+  const options = {
+    expiresIn: '5hr', 
+    jwtid: '340952349066'
+  }
+
+  return jwt.sign(payload, secret, options); 
 }
+
+function validateRequest(userInput){
+  const schema = {
+    username: Joi.string().min(6).alphanum().max(30).required(),
+    password: Joi.string().min(8).required()
+  }
+  return Joi.validate(userInput, schema)
+}
+
+function register(req, res) {
+  const creds = req.body; 
+
+  const validated = validateRequest(creds)
+  if(validated.error){
+    res.status(404).json(validated.error.details)
+    return; 
+  }
+
+  const hash = bcrypt.hashSync(creds.password, 12); 
+  creds.password = hash;
+
+  db('users')
+    .insert(creds)
+    .then(id => {
+      const token = generateToken(creds.username); 
+      res.status(201).json({id, token}); 
+  }).catch(err => {
+    res.status(500).json({error: "Could not update data in the database"})
+  })
+}
+
 
 function login(req, res) {
   // implement user login
