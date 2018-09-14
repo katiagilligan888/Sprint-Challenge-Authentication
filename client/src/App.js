36111f3823d23ce6456df6bd458eca54e4c4a9e4
Katherine Gilligan <katia.gilligan88@gmail.com>
2018-09-14 11:27:37 -0600

    Added Registration component with local Storage

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
+import {Route} from 'react-router-dom'; 
+import Register from './Components/Register'; 
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-      
+        <Route path = "/register" component = {Register} />
       </div>
     );
   }
---
+++ client/src/Components/Register.js
@@ -0,0 +1,62 @@
+import React from "react";
+import axios from "axios";
+
+class Register extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      username: "",
+      password: ""
+    };
+  }
+
+  onInputChangeHandler = event => {
+    this.setState({
+      [event.target.name]: event.target.value
+    });
+  };
+
+  onSubmitHandler = event => {
+    event.preventDefault();
+    axios
+      .post("http://localhost:3300/api/register", this.state)
+      .then(response => {
+        this.setState({
+          username: "",
+          password: ""
+        });
+        const token = response.data.token;
+        localStorage.setItem("jwt", token);
+        //    this.props.history.push('/jokes');
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  };
+
+  render() {
+    return (
+      <div className="register">
+        <form onSubmit={this.onSubmitHandler} className="sign-up-form">
+          <input
+            onChange={this.onInputChangeHandler}
+            type="text"
+            placeholder="Username"
+            name="username"
+            value={this.state.username}
+          />
+          <input
+            onChange={this.onInputChangeHandler}
+            type="password"
+            placeholder="Password"
+            name="password"
+            value={this.state.password}
+          />
+          <button type="submit">Sign up!</button>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default Register;
